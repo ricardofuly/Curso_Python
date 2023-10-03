1a1225fac870c996110947c33571339eac83f0dc
Ricardo <lricardofs@gmail.com>
2023-10-03 09:59:47 -0300

    Tipos Primitivos.

---
+++ tipos_primitivos.py
@@ -0,0 +1,6 @@
+x = input('Digite alguma coisa: ')
+
+print('é um numero: ', x.isalnum())
+print('é uma string: ', x.isalpha())
+print('esta em maiusculo: ', x.isupper())
+print('esta em maiusculo: ', x.islower())
