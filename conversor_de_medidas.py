194fb4a6c583099f131fb5b4fc054935fee8b3f2
Ricardo <lricardofs@gmail.com>
2023-10-03 12:00:33 -0300

    adicionado as convesões para as medidas de dc, dam, hec.

--- conversor_de_medidas.py
+++ conversor_de_medidas.py
@@ -1,7 +1,10 @@
 medida = float(input('Digite a sua medida em metros: '))
+dc = medida * 10
 cm = medida * 100
 mm = medida * 1000
+dam = medida / 10
+hec = medida / 100
 km = medida / 1000
 
-print('A medida de {}m corresponde a \n{:.0f}cm \n{:.0f}mm \n{}km'.format(medida, cm, mm, km))
+print('A medida de {}m corresponde a \n{:.0f}dc \n{:.0f}cm \n{:.0f}mm \n{:.0f}dam \n{:.0f}hec \n{}km'.format(medida, dc, cm, mm, dam, hec, km))
 
