ba48c9c96496c8bbfce2e2bd76590b0acb9613d3
Ricardo <lricardofs@gmail.com>
2023-10-03 11:26:35 -0300

    Calculo de media

---
+++ operadores_aritimeticos.py
@@ -0,0 +1,7 @@
+n1 = float(input('Digite a nota primeira nota: '))
+n2 = float(input('Digite a nota primeira nota: '))
+
+resultado = (n1 + n2) /2
+
+print('A media entre a nota {:.1f} e {:.1f} foi: {}'.format(n1, n2, resultado))
+
